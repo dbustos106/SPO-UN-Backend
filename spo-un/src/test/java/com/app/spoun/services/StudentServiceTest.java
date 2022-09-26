9b8a46e91f9a2747c0352d988a5f96a059431c42
dbustos106 <51845030+dbustos106@users.noreply.github.com>
2022-09-26 03:58:33 -0500

    add comment

---
+++ spo-un/src/test/java/com/app/spoun/services/StudentServiceTest.java
@@ -0,0 +1,17 @@
+package com.app.spoun.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveStudent() {
+    }
+}
